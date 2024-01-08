92e7e5f61492afc70271cbd829e20ce3a344fea9
Abu Bakar <abubakarzafar2029@gmail.com>
2024-01-08 20:22:12 +0500

    React with tpeScript module completed

--- React_With_TypeScript/reminder-app/src/App.tsx
+++ React_With_TypeScript/reminder-app/src/App.tsx
@@ -1,21 +1,37 @@
 import "./App.css";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Reminder from "./entities/reminder";
 import ReminderList from "./components/ReminderList/ReminderList";
+import { reminderService } from "./services/reminder";
+import NewReminder from "./components/NewReminder/NewReminder";
 
 function App() {
-  // const [reminders, setReminders] = useState<Reminder[]>(); // showing ( reminders[] | undefined )
-  const [reminders, setReminders] = useState<Reminder[]>([
-    {
-      id: 1,
-      title: "Reminder 1",
-    },
-  ]);
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await reminderService.getReminders();
+
+    setReminders(reminders);
+  };
+
+  const removeReminder = (id: number): void => {
+    setReminders(reminders.filter((reminder) => reminder.id !== id));
+  };
+
+  const addReminder = async (title: string) => {
+    console.log(title);
+    const newReminder = await reminderService.addReminder(title);
+    setReminders([newReminder, ...reminders]);
+  };
   return (
     <div className="App">
-      <ReminderList items={reminders} />
+      <NewReminder onAddReminder={addReminder} />
+      <ReminderList items={reminders} onRemoveReminder={removeReminder} />
     </div>
   );
 }
--- React_With_TypeScript/reminder-app/src/components/ReminderList/ReminderList.tsx
+++ React_With_TypeScript/reminder-app/src/components/ReminderList/ReminderList.tsx
@@ -2,14 +2,24 @@ import Reminder from "../../entities/reminder";
 
 interface ReminderListProps {
   items: Reminder[];
+  onRemoveReminder: (id: number) => void;
 }
 
 // function ReminderList(props: ReminderListProps) {
-function ReminderList({ items }: ReminderListProps) {
+function ReminderList({ items, onRemoveReminder }: ReminderListProps) {
   return (
-    <ul>
+    <ul className="list-group">
       {items.map((item) => (
-        <li key={item.id}>{item.title}</li>
+        <li key={item.id} className="list-group-item">
+          {item.title}
+          <button
+            type="button"
+            className="btn btn-outline-primary mx-2 rounded-pill"
+            onClick={()=>onRemoveReminder(item.id)}
+          >
+            Delete
+          </button>
+        </li>
       ))}
     </ul>
   );
